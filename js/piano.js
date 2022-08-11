e798f2815dc7ddaa459e618bd948ae907c53eabf
Gabriel Alexsandro <gabrielalexsandro2011@gmail.com>
2022-08-11 07:51:46 -0300

    #fixed-bug last and first octave note

--- js/piano.js
+++ js/piano.js
@@ -1,5 +1,5 @@
 const teclas = document.querySelectorAll(".oitava > div");
-const dictTeclas = {'c':0, 'cs':1, 'd':2, 'ds':3, 'e':4, 'es':5, 'f':6, 'fs':7, 'g':8, 'gs':9, 'a':10, 'as':11, 'b':12};
+const dictTeclas = {'c':0, 'cs':1, 'd':2, 'ds':3, 'e':4, 'f':5, 'fs':6, 'g':7, 'gs':8, 'a':9, 'as':10, 'b':11};
 let teclasTocando = [];
 
 // if(deviceIsMobile){
